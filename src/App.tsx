4af670b299f2603ba29911e19a8fe715649dd985
Achyuth KP <kpachyuthz@gmail.com>
2025-12-17 21:26:27 +0530

    Switch to HashRouter for consistent GH Pages routing

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 import ErrorBoundary from "./components/ErrorBoundary";
 import PremiumLoader from "@/components/PremiumLoader";
 import SmoothScroll from "./components/ui/SmoothScroll";
@@ -22,7 +22,7 @@ const App = () => (
         <Sonner />
         <CustomCursor />
         <ScrollProgress />
-        <BrowserRouter basename={import.meta.env.BASE_URL}>
+        <HashRouter>
           <SmoothScroll>
             <Routes>
               <Route path="/" element={<Index />} />
@@ -30,7 +30,7 @@ const App = () => (
               <Route path="*" element={<NotFound />} />
             </Routes>
           </SmoothScroll>
-        </BrowserRouter>
+        </HashRouter>
       </TooltipProvider>
     </QueryClientProvider>
   </ErrorBoundary>
